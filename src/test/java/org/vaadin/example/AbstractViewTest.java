6f3b2c67f8aa4e859e766e7b1492c090bb90ddb4
marcin <106965834+MarcinVaadin@users.noreply.github.com>
2022-11-10 14:34:11 +0100

    unsolved examples

--- src/test/java/org/vaadin/example/AbstractViewTest.java
+++ src/test/java/org/vaadin/example/AbstractViewTest.java
@@ -2,17 +2,7 @@ package org.vaadin.example;
 
 import org.junit.jupiter.api.BeforeEach;
 
-import com.vaadin.testbench.BrowserTestBase;
-import com.vaadin.testbench.annotations.RunLocally;
-import com.vaadin.testbench.browser.BrowserTestInfo;
-import com.vaadin.testbench.parallel.Browser;
-
-/**
- * Test step #2 - extend TestBench JUnit 5 parent utility class and remember to
- * open test page before running test
- */
-@RunLocally(Browser.CHROME) // Test step #6 - define specific browser
-public abstract class AbstractViewTest extends BrowserTestBase {
+public abstract class AbstractViewTest {
 
     // update if required
     private static final String TEST_URL = "http://localhost:8080";
@@ -20,8 +10,8 @@ public abstract class AbstractViewTest extends BrowserTestBase {
     // before each test it is required to open the test page using
     // webdriver injected by TestBench
     @BeforeEach
-    public void beforeEach(BrowserTestInfo browserTestInfo) {
-        browserTestInfo.driver().get(TEST_URL);
+    public void beforeEach() {
+
     }
 
 }
--- src/test/java/org/vaadin/example/MainViewIT.java
+++ src/test/java/org/vaadin/example/MainViewIT.java
@@ -1,19 +1,5 @@
 package org.vaadin.example;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
-import org.junit.jupiter.api.Assertions;
-
-import com.vaadin.flow.component.button.testbench.ButtonElement;
-import com.vaadin.flow.component.html.testbench.ParagraphElement;
-import com.vaadin.flow.component.textfield.testbench.TextFieldElement;
-import com.vaadin.testbench.BrowserTest;
-import com.vaadin.testbench.browser.BrowserTestInfo;
-import com.vaadin.testbench.parallel.BrowserUtil;
-
-/**
- * Test step #2 - make nameEntered_greetingPresent test run using TestBench
- */
 public class MainViewIT extends AbstractViewTest {
 
     private static final String THE_NAME = "Grzegorz";
@@ -22,52 +8,33 @@ public class MainViewIT extends AbstractViewTest {
 
     private static final String THE_AGE = "35";
 
-    // Greeting base test
-    @BrowserTest
-    public void nameEntered_greetingPresent() {
-        $(TextFieldElement.class).id("name").setValue(THE_NAME);
-        $(ButtonElement.class).first().click();
-        String value = $(ParagraphElement.class).first().getText();
-
-        Assertions.assertEquals("Hello " + THE_NAME, value);
-    }
-
-    // Test step #3 - add additional test methods for running in parallel
-    @BrowserTest
-    public void nameSubmitted_nameIsOk() {
-        $(TextFieldElement.class).id("name").setValue(THE_NAME);
-        $(ButtonElement.class).first().click();
-        String value = $(TextFieldElement.class).id("nameValue").getValue();
-
-        Assertions.assertEquals(THE_NAME, value);
-
-        // Test step #5 - rerunning flaky test
-        // rerunFailingTestsCount = 3, so pass on 3
-        Assertions.assertEquals(3, someRetryCounter.getAndIncrement());
-    }
-    private static final AtomicInteger someRetryCounter = new AtomicInteger(1);
-
-    @BrowserTest
-    public void lastnameSubmitted_lastnameIsOk(BrowserTestInfo browserTestInfo) {
-
-        // Test step #6 - check browser that is used in test
-        System.out.println(browserTestInfo.runLocallyBrowser());
-        Assertions.assertTrue(BrowserUtil.isChrome(browserTestInfo.capabilities()));
-
-        $(TextFieldElement.class).id("lastname").setValue(THE_LAST_NAME);
-        $(ButtonElement.class).first().click();
-        String value = $(TextFieldElement.class).id("lastnameValue").getValue();
-
-        Assertions.assertEquals(THE_LAST_NAME, value);
-    }
-
-    @BrowserTest
-    public void ageSubmitted_ageIsOk() {
-        $(TextFieldElement.class).id("age").setValue(THE_AGE);
-        $(ButtonElement.class).first().click();
-        String value = $(TextFieldElement.class).id("ageValue").getValue();
-
-        Assertions.assertEquals(THE_AGE, value);
-    }
+    // some example tests below, not to waste time for writing them
+//    public void nameEntered_greetingPresent() {
+//        $(TextFieldElement.class).id("name").setValue(THE_NAME);
+//        $(ButtonElement.class).first().click();
+//        String value = $(ParagraphElement.class).first().getText();
+//        Assertions.assertEquals("Hello " + THE_NAME, value);
+//    }
+//
+//    public void nameSubmitted_nameIsOk() {
+//        $(TextFieldElement.class).id("name").setValue(THE_NAME);
+//        $(ButtonElement.class).first().click();
+//        String value = $(TextFieldElement.class).id("nameValue").getValue();
+//        Assertions.assertEquals(THE_NAME, value);
+//    }
+//
+//    public void lastnameSubmitted_lastnameIsOk() {
+//        $(TextFieldElement.class).id("lastname").setValue(THE_LAST_NAME);
+//        $(ButtonElement.class).first().click();
+//        String value = $(TextFieldElement.class).id("lastnameValue").getValue();
+//        Assertions.assertEquals(THE_LAST_NAME, value);
+//    }
+//
+//    public void ageSubmitted_ageIsOk() {
+//        $(TextFieldElement.class).id("age").setValue(THE_AGE);
+//        $(ButtonElement.class).first().click();
+//        String value = $(TextFieldElement.class).id("ageValue").getValue();
+//        Assertions.assertEquals(THE_AGE, value);
+//    }
 
 }
