e5656ba48347584256e17e026142ba7ba0039836
marcin <106965834+MarcinVaadin@users.noreply.github.com>
2022-11-10 14:27:25 +0100

    solved examples

--- src/main/java/org/vaadin/example/MainView.java
+++ src/main/java/org/vaadin/example/MainView.java
@@ -1,74 +1,64 @@
 package org.vaadin.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootVersion;
-import org.springframework.core.SpringVersion;
 
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Paragraph;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
 /**
  * A sample Vaadin view class.
- * <p>
- * To implement a Vaadin view just extend any Vaadin component and use @Route
- * annotation to announce it in a URL as a Spring managed bean.
- * <p>
- * A new instance of this class is created for every new user and every browser
- * tab/window.
- * <p>
- * The main view contains a text field for getting the user name and a button
- * that shows a greeting message in a notification.
  */
 @Route
-@CssImport("./styles/shared-styles.css")
-@CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
 public class MainView extends VerticalLayout {
 
-    /**
-     * Construct a new Vaadin view.
-     * <p>
-     * Build the initial UI state for the user accessing the application.
-     *
-     * @param service
-     *            The message service. Automatically injected Spring managed
-     *            bean.
-     */
     public MainView(@Autowired GreetService service) {
 
-        // Use TextField for standard text input
-        TextField textField = new TextField("Your name");
-        textField.addThemeName("bordered");
+        setWidth("300px");
+        getStyle().set("margin", "0 auto");
 
-        // Button click listeners can be defined as lambda expressions
-        Button button = new Button("Say hello",
-                e -> Notification.show(service.greet(textField.getValue())));
+        TextField nameField = createTextField("Enter name", "name", false);
+        TextField lastNameField = createTextField("Enter lastname", "lastname", false);
+        TextField ageField = createTextField("Enter age", "age", false);
 
-        // Theme variants give you predefined extra styles for components.
-        // Example: Primary button has a more prominent look.
+        H1 h1 = new H1("Welcome to DX Tests");
+
+        TextField nameFieldValue = createTextField("Name:", "nameValue", true);
+        TextField lastNameFieldValue = createTextField("Lastname:", "lastnameValue", true);
+        TextField ageFieldValue = createTextField("Age:", "ageValue", true);
+
+        FormLayout formLayout = new FormLayout();
+        formLayout.add(nameField, lastNameField, ageField);
+
+        Paragraph serviceResponse = new Paragraph("");
+
+        Button button = new Button("Submit",
+                e -> {
+                    nameFieldValue.setValue(nameField.getValue());
+                    lastNameFieldValue.setValue(lastNameField.getValue());
+                    ageFieldValue.setValue(String.valueOf(ageField.getValue()));
+                    serviceResponse.setText(service.greet(nameField.getValue()));
+                });
         button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-
-        // You can specify keyboard shortcuts for buttons.
-        // Example: Pressing enter in this view clicks the Button.
         button.addClickShortcut(Key.ENTER);
 
-        // Use custom CSS classes to apply styling. This is defined in
-        // shared-styles.css.
-        addClassName("centered-content");
+        formLayout.add(button);
+        formLayout.add(nameFieldValue, lastNameFieldValue, ageFieldValue);
 
-        Paragraph springVersion = new Paragraph(
-                "Spring version = " + SpringVersion.getVersion());
+        add(h1, formLayout, serviceResponse);
+    }
 
-        Paragraph springBootVersion = new Paragraph(
-                "Spring Boot version = " + SpringBootVersion.getVersion());
-
-        add(textField, button, springVersion, springBootVersion);
+    private TextField createTextField(String label, String id, boolean readOnly) {
+        TextField field = new TextField(label);
+        field.setId(id);
+        field.setReadOnly(readOnly);
+        return field;
     }
 
 }
